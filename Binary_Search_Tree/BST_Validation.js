3453671ccddb2a8fee17552bc79fec313f7adc3a
joecaro <jsc11598@gmail.com>
2021-04-21 22:56:47 -0400

    BST and Test

---
+++ Binary_Search_Tree/BST_Validation.js
@@ -0,0 +1,23 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function validateBst(tree) {
+  return validateBstHelper(tree, -Infinity, Infinity);
+}
+
+function validateBstHelper(tree, minValue, maxValue) {
+  if (tree === null) return true;
+  if (tree.value < minValue || tree.value >= maxValue) return false;
+  const leftIsValid = validateBstHelper(tree.left, minValue, tree.value);
+  const rightIsValid = validateBstHelper(tree.right, tree.value, maxValue);
+  return leftIsValid && rightIsValid;
+}
+
+// Do not edit the line below.
+exports.BST = BST;
+exports.validateBst = validateBst;
---
+++ Tests/BST_Validation.js
@@ -0,0 +1,73 @@
+const program = require("../Binary_Search_Tree/BST_Validation");
+
+class BST extends program.BST {
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+    return this;
+  }
+
+  insertArray(array) {
+    array.forEach((value) => {
+      if (value < this.value) {
+        if (this.left === null) {
+          this.left = new BST(value);
+        } else {
+          this.left.insert(value);
+        }
+      } else {
+        if (this.right === null) {
+          this.right = new BST(value);
+        } else {
+          this.right.insert(value);
+        }
+      }
+    });
+    return this;
+  }
+}
+
+// let array = [];
+
+// function getArray(tree) {
+//   if (tree === null) return;
+//   array.push(tree.value);
+//   getArray(tree.left);
+//   getArray(tree.right);
+// }
+
+let tests = [
+  {
+    test: new BST(5).insertArray([1, 2, 4, 2, 5, 23, 234, 6, 10]),
+    expected: true,
+  },
+  {
+    test: new BST(5).insertArray([1, 2, 4, 8, 5, 5, 10]),
+    expected: true,
+  },
+  {
+    test: new BST(5).insertArray([1, 3, 7, 4, 90]),
+    expected: true,
+  },
+];
+
+function testTrees(array) {
+  array.forEach((test) => {
+    console.log(
+      program.validateBst(test.test) === test.expected ? "PASSED √" : "FAILED X"
+    );
+  });
+}
+
+testTrees(tests);
